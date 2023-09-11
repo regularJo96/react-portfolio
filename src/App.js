95bfe5bb705f73ba326d12b32a2aa8375a0aeaf0
Josiah Anderson <riddlerforever@live.com>
2023-09-11 00:34:27 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import './assets/style-remaster.css';
+
+import me from './assets/images/me.png';
+import image1 from './assets/images/me-at-desk-1.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="disclaimer">CONSTRUCTION</div>
+    <div className="center"></div>
+    <div id="container-1" className="container-1">
+        <div className="name text-border-black">
+            <div className="greeting">
+                Hi, I'm
+            </div>
+            Josiah Anderson
+        </div>
+
+        <div className='menu font-18'>
+        <div id="Software">
+          <p className='text-center text-border-black'>
+            Software Engineer
+          </p>
+        </div>
+        <div id="Cs">
+          <p className='text-center text-border-black'>
+            Computer Scientist
+          </p>
+        </div>
+        <div id="Gamer">
+          <p className='text-center text-border-black'>
+            Pro-Gamer
+          </p>
+        </div>
+        <div id="Reader">
+          <p className='text-center text-border-black'>
+            Casual Reader
+          </p>
+        </div>
+        <div id="Coffee">
+          <p className='text-center text-border-black'>
+            Coffee
+          </p>
+        </div>
+        <div id="Tortilla">
+          <p className='text-center text-border-black'>
+            Tortillas
+          </p>
+        </div>
+        </div>
+        <div id="picture-1" className="picture">
+            <img id="background" src={me}/>
+        </div>
+    </div>
+
+    <div className="disclaimer">This site is under construction.</div>
     </div>
   );
 }
